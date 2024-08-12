33c8c37d9053dfec2dc26b118a53edaedb2259bd
madscience2728 <madscience2728@gmail.com>
2024-08-12 05:21:46 -0400

    Fixed bat files so they don't use absolute paths

--- play_the_game_publish.bat
+++ play_the_game_publish.bat
@@ -1,3 +1,3 @@
 dotnet publish
-"C:\Users\madsc\Data\Files\Visual Studio Code Projects\Sandbox City Simulator 2024\bin\Debug\net8.0\Sandbox City Simulator 2024.exe"
+".\bin\Debug\net8.0\Sandbox City Simulator 2024.exe"
 pause
